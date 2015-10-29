e6939366695f1594db988b8330d8250c34224095
Matias K <matias@adtile.me>
2015-10-29 13:26:42 +0200

    Point project url to correct place, bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-lambda-event-s3 "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-lambda-event-s3 "0.1.1"
+  :description "A Clojure library designed for parsing AWS Lambda S3 events"
+  :url "https://github.com/adtile/clj-lambda-event-s3"
   :license {:name "MIT"}
   :signing {:gpg-key "webmaster@adtile.me"}
   :dependencies [[org.clojure/clojure "1.7.0"]
