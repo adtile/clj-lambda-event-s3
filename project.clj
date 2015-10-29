a6edf9473c96f5e86cbdc691fbbdd8f82b933c77
Markus Hjort <markus.hjort@gmail.com>
2015-10-29 13:16:58 +0200

    Configure gpg-key for signing Clojars releases

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "MIT"}
+  :signing {:gpg-key "webmaster@adtile.me"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [amazonica/amazonica "0.3.37"]
                  [prismatic/schema "0.4.4"]])
