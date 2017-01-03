c9335a3d4a8c592cb4706702cac7e3f611a25fc3
Jaakko Suutarla <jaakko@suutarla.com>
2017-01-03 13:39:24 +0200

    Release 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-lambda-event-s3 "0.1.4"
+(defproject clj-lambda-event-s3 "1.0.0"
   :description "A Clojure library designed for parsing AWS Lambda S3 events"
   :url "https://github.com/adtile/clj-lambda-event-s3"
   :license {:name "MIT"}
