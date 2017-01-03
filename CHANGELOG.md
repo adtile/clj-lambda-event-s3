311520860a8bb9c37dd77d12a4fbe75be502d96d
Jaakko Suutarla <jaakko@suutarla.com>
2017-01-03 15:48:00 +0200

    Fallback to older AWS S3 SDK

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [1.0.0] - 2016-01-03
+## [1.0.1] - 2017-01-03
+### Changed
+- Fallback to older version of ```[com.amazonaws/aws-java-sdk-s3 "1.11.15"]``` because of changed checksum calculations.
+
+## [1.0.0] - 2017-01-03
 ### Added
 - Replace Amazonica with AWS SDK
--- project.clj
+++ project.clj
@@ -1,13 +1,11 @@
-(defproject clj-lambda-event-s3 "1.0.0"
+(defproject clj-lambda-event-s3 "1.0.1"
   :description "A Clojure library designed for parsing AWS Lambda S3 events"
   :url "https://github.com/adtile/clj-lambda-event-s3"
   :license {:name "MIT"}
   :signing {:gpg-key "webmaster@adtile.me"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.75"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.15"]
                  [clj-time "0.13.0"]
                  [prismatic/schema "1.1.3"]]
   :profiles {:dev {:dependencies [[clj-containment-matchers "1.0.1"]]
-                   :jvm-opts ["-Dcom.amazonaws.services.s3.disablePutObjectMD5Validation=true"
-                              "-Dcom.amazonaws.services.s3.disableGetObjectMD5Validation=true"]
                    :resource-paths ["dev-resources"]}})
